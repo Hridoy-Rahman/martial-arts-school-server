4883e4d204f3d80290fb9f1cb5dec6588ece7a43
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-06-09 12:03:06 +0600

    classes collection

--- index.js
+++ index.js
@@ -8,6 +8,67 @@ app.use(cors());
 app.use(express())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRETE_KEY}@cluster0.ey6jdyf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRETE_KEY}@cluster0.ey6jdyf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const classesCollection=client.db("martialArts").collection("classes")
+
+
+
+    app.get("/classes",async(req,res)=>{
+        const result=await classesCollection.find().toArray();
+        res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/',(req,res)=>{
     res.send('Server is running')
 })
